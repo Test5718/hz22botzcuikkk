b33487008dc8bbf6cb54458f51800aab4474e985
Test5718 <106467943+Test5718@users.noreply.github.com>
2022-07-06 19:06:53 +0700

    Update asupans.js

--- plugins/asupans.js
+++ plugins/asupans.js
@@ -1,11 +1,11 @@
-=  
-	./random', 'asupan.mp4', '𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢AzzBot\nSubscribe : https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g', m)
+let handler = async (m, { conn }) => {
+	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/random', 'asupan.mp4', '𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢Hz22Botz\nSubscribe : https://youtube.com/channel/hz22yt', m)
 }
 handler.help = ['asupan']
+handler.tags = ['asupan']
 
-
-
-
-
-
-
+handler.command = /^(asupan)$/i
+handler.premium = false
+handler.register = true
+handler.limit = 5
+module.exports = handler
